853158b3dee0b34394f4c3b0e9173216ede7189a
Olivers <bindris.olivers@gmail.com>
2025-10-22 11:28:12 +0300

    new file

---
+++ problem.js
@@ -0,0 +1,12 @@
+//let club = [
+   //(Name="Adolf",Surname = "Brown", Class= "Leader", Hours= "200")
+   //(Name="Arnold",Surname = "Shwarznegger", Class= "Leader", Hours= "100")
+   //(Name="Emma",Surname = "Flower", Class= "Member", Hours= "46")
+   //(Name="Robert",Surname = "Manastia", Class= "Member", Hours= "23")
+  // (Name="Aron",Surname = "Kingstein", Class= "Member", Hours= "3")
+  // (Name="Mantas",Surname = "Patvaiskis", Class= "Member", Hours= "4")
+   //(Name="Janis",Surname = "Ozols", Class= "Member", Hours= "19")
+//]
+console.log("Adolf Brown and Arnold Shwarznegger are the leaders")
+console.log("395 Hours")
+console.log("Star volunteer's are Emma Flower and Robert Manastia")
